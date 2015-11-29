465c823630ddec298e4d0a7e093931e958622569
James Hunt <james@niftylogic.com>
2015-11-29 13:19:37 -0600

    Colorized debugging / trace output

--- assets/app.js
+++ assets/app.js
@@ -15,6 +15,66 @@ $(function () {
 		}
 	})
 
+	var wrap = function (s) {
+		var indent = "";
+		var toknum = 0;
+		for (var i = 0; i < s.length; i++) {
+			indent += ' ';
+			if (s[i] == ' ' && toknum == 0) {
+				toknum++;
+				continue;
+			}
+			if (s[i] != ' ' && toknum == 1) {
+				break;
+			}
+		}
+
+		var lines = [];
+		var buf = "";
+		var tokens = s.split(/[ \r\t\n]/);
+		for (var i = 0; i < tokens.length; i++) {
+			if (buf.length + tokens[i].length > 80) {
+				lines.push(buf);
+				buf = tokens[i];
+			} else {
+				buf += ' ' + tokens[i];
+			}
+		}
+		if (buf.length > 0) {
+			lines.push(buf);
+		}
+		return lines.join("<br/>" + indent + " <span class=\"cont\">&#8594;</span> ");
+	}
+
+	// colorize debugging output
+	var colorize = function (s) {
+		out = []
+		lines = s.split("\n");
+		for (i = 0; i < lines.length; i++) {
+			var line = lines[i];
+			var html = wrap(line);
+			if (line.match(/^--+> data flow:/)) {
+				out.push('<span class="data-flow">'+html+'</span>');
+			} else if (line.match(/^--+>\s*$/)) {
+				out.push('<span class="trace">&nbsp;</span>');
+			} else if (line.match(/^--+>/)) {
+				out.push('<span class="trace">'+html+'</span>');
+
+			} else if (line.match(/^DEBUG> parsing/)) {
+				out.push('<span class="debug debug-parsing">'+html+'</span>');
+			} else if (line.match(/^DEBUG> running/)) {
+				out.push('<span class="debug debug-running">'+html+'</span>');
+			} else if (line == 'DEBUG> ') {
+				out.push('<span class="debug">&nbsp;</span>');
+			} else if (line.match(/^DEBUG>/)) {
+				out.push('<span class="debug">'+html+'</span>');
+			} else {
+				out.push('<span>'+html+'</span>');
+			}
+		}
+		return out.join("");
+	}
+
 	// turn a prune list (text) into a list of parsed keys
 	// (ignoring comments and blank lines).
 	var prune = function (s) {
@@ -67,7 +127,20 @@ $(function () {
 				debug : $('#debug').is(':checked'),
 				trace : $('#trace').is(':checked'),
 				prune : prune($('#prune').val())
-			})
+			}),
+			success: function(raw) {
+				data = JSON.parse(raw);
+				console.dir(data);
+
+				$('#stdout, #stderr').hide();
+
+				if (data.stdout != "") {
+					$('#stdout').html(data.stdout).show();
+				}
+				if (data.stderr != "") {
+					$('#stderr').html(colorize(data.stderr)).show();
+				}
+			}
 		});
 	})
 })
