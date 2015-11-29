71c0389501a220d239f9afbbc2ed1a08be4e79a5
James Hunt <james@niftylogic.com>
2015-11-29 13:47:42 -0600

    Refactor `spruce' exec into spruce.go

---
+++ spruce.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type Result struct {
+	Arguments []string `json:"args"`
+	Stdout    string   `json:"stdout"`
+	Stderr    string   `json:"stderr"`
+	Success   bool     `json:"success"`
+}
+
+func Spruce(where string, args ...string) (*Result, error) {
+	r := &Result{Arguments: args}
+
+	cmd := exec.Command("spruce", args...)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Printf("failed to get stdout pipe from command: %s", err)
+		return nil, err
+	}
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Printf("failed to get stderr pipe from command: %s", err)
+		return nil, err
+	}
+
+	err = os.Chdir(where)
+	if err != nil {
+		log.Printf("failed to chdir to %s: %s", where, err)
+		return nil, err
+	}
+
+	log.Printf("running `spruce %v' in %s", args, where)
+	err = cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	output := make(chan string)
+	errors := make(chan string)
+	drain := func(rd io.Reader, out chan string) {
+		var buf []string
+		s := bufio.NewScanner(rd)
+		for s.Scan() {
+			buf = append(buf, s.Text()+"\n")
+		}
+		out <- strings.Join(buf, "")
+		close(out)
+	}
+	go drain(stdout, output)
+	go drain(stderr, errors)
+
+	err = cmd.Wait()
+	r.Success = (err != nil)
+	r.Stdout = <-output
+	r.Stderr = <-errors
+	return r, nil
+}
+
+func SpruceVersion() (string, error) {
+	r, err := Spruce("", "version")
+	if err != nil {
+		return "", err
+	}
+	return r.Stdout, nil
+}
+
+type Merge struct {
+	Prune []string
+	YAML  []struct {
+		Filename string
+		Contents string
+	}
+
+	Debug bool
+	Trace bool
+}
+
+func SpruceMerge(m Merge) (*Result, error) {
+	dir, err := ioutil.TempDir("", "web")
+	if err != nil {
+		log.Printf("failed to create temporary working directory: %s", err)
+		return nil, err
+	}
+	// defer rm() the dir
+
+	var args []string
+	if m.Debug {
+		args = append(args, "--debug")
+	}
+	if m.Trace {
+		args = append(args, "--trace")
+	}
+	args = append(args, "merge")
+
+	if len(m.Prune) > 0 {
+		args = append(args, "--prune")
+		for _, f := range m.Prune {
+			args = append(args, f)
+		}
+	}
+	for _, y := range m.YAML {
+		args = append(args, y.Filename)
+		ioutil.WriteFile(
+			fmt.Sprintf("%s/%s", dir, y.Filename),
+			[]byte(y.Contents),
+			os.FileMode(0400),
+		)
+	}
+
+	return Spruce(dir, args...)
+}
--- web.go
+++ web.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
-	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/exec"
-	"strings"
 )
 
 func main() {
@@ -41,16 +36,7 @@ func main() {
 			return
 		}
 
-		var params struct {
-			Prune []string
-			YAML  []struct {
-				Filename string
-				Contents string
-			}
-
-			Debug bool
-			Trace bool
-		}
+		var params Merge
 		err = json.Unmarshal(all, &params)
 		if err != nil {
 			log.Printf("failed to unmarshal JSON payload: %s", err)
@@ -60,97 +46,13 @@ func main() {
 			return
 		}
 
-		dir, err := ioutil.TempDir("", "web")
-		if err != nil {
-			log.Printf("failed to create temporary working directory: %s", err)
-			w.WriteHeader(500)
-			w.Write([]byte("internal server error"))
-			return
-		}
-		// defer rm() the dir
-
-		var args []string
-		if params.Debug {
-			args = append(args, "--debug")
-		}
-		if params.Trace {
-			args = append(args, "--trace")
-		}
-		args = append(args, "merge")
-
-		if len(params.Prune) > 0 {
-			args = append(args, "--prune")
-			for _, f := range params.Prune {
-				args = append(args, f)
-			}
-		}
-		for _, y := range params.YAML {
-			args = append(args, y.Filename)
-			ioutil.WriteFile(
-				fmt.Sprintf("%s/%s", dir, y.Filename),
-				[]byte(y.Contents),
-				os.FileMode(0400),
-			)
-		}
-
-		cmd := exec.Command("spruce", args...)
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Printf("failed to get stdout pipe from command: %s", err)
-			w.WriteHeader(500)
-			w.Write([]byte("internal server error"))
-			return
-		}
-		stderr, err := cmd.StderrPipe()
-		if err != nil {
-			log.Printf("failed to get stderr pipe from command: %s", err)
-			w.WriteHeader(500)
-			w.Write([]byte("internal server error"))
-			return
-		}
-
-		err = os.Chdir(dir)
+		result, err := SpruceMerge(params)
 		if err != nil {
 			w.WriteHeader(500)
 			w.Write([]byte("internal server error"))
 			return
 		}
-
-		log.Printf("running `spruce %v' in %s", args, dir)
-		err = cmd.Start()
-		if err != nil {
-			w.WriteHeader(500)
-			w.Write([]byte("internal server error"))
-			return
-		}
-
-		output := make(chan string)
-		errors := make(chan string)
-		drain := func(rd io.Reader, out chan string) {
-			var buf []string
-			s := bufio.NewScanner(rd)
-			for s.Scan() {
-				buf = append(buf, s.Text()+"\n")
-			}
-			out <- strings.Join(buf, "")
-			close(out)
-		}
-		go drain(stdout, output)
-		go drain(stderr, errors)
-
-		err = cmd.Wait()
-		var response struct {
-			Arguments []string `json:"args"`
-			Stdout    string   `json:"stdout"`
-			Stderr    string   `json:"stderr"`
-			Success   bool     `json:"success"`
-		}
-		response.Success = (err != nil)
-		response.Arguments = args
-		response.Stdout = <-output
-		response.Stderr = <-errors
-
-		m, err := json.Marshal(response)
+		m, err := json.Marshal(result)
 		if err != nil {
 			log.Printf("failed to marshal JSON response: %s", err)
 			w.WriteHeader(500)
