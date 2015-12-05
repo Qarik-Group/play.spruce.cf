74ec8ac993cc5bd612298da1fc0abaa4f83c2c8c
James Hunt <james@niftylogic.com>
2015-12-05 10:41:00 -0600

    Allow ./ci/pipeline.sh to be run from anywhere

--- ci/pipeline.sh
+++ ci/pipeline.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+root=$(cd $(dirname $BASH_SOURCE[0])/.. && pwd)
+cd ${root}/ci
+
 spruce --concourse merge --prune meta global.yml docker.yml deploy.yml > pipeline.yml
 fly -t tutorial c play.spruce.cf -c pipeline.yml --vars-from credentials.yml
